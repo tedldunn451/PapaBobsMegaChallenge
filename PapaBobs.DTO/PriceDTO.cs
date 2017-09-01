6f3b4fd5c266512e6a095f44a60ab7698d64de7f
Ted Dunn <tldunn451@gmail.com>
2017-09-01 12:49:32 -0400

    Add Price repository

---
+++ PapaBobs.DTO/PriceDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs.DTO
+{
+	public class PriceDTO
+	{
+		public int Id { get; set; }
+		public decimal SmallSizePrice { get; set; }
+		public decimal MediumSizePrice { get; set; }
+		public decimal LargeSizePrice { get; set; }
+		public decimal RegularCrustPrice { get; set; }
+		public decimal ThinCrustPrice { get; set; }
+		public decimal ThickCrustPrice { get; set; }
+		public decimal SausagePrice { get; set; }
+		public decimal PepperoniPrice { get; set; }
+		public decimal OnionsPrice { get; set; }
+		public decimal GreenPeppersPrice { get; set; }
+	}
+}
--- PapaBobs.Data/PriceRepository.cs
+++ PapaBobs.Data/PriceRepository.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace PapaBobs.Data
 {
-	class PriceRepository
+	public class PriceRepository
 	{
+		public static DTO.PriceDTO GetPrices()
+		{
+			var db = new PapaBobsDbEntities();
+			var prices = db.Prices.First();
+			var dtoPrice = convertToDTO(prices);
+
+			return dtoPrice;
+		}
+
+		private static DTO.PriceDTO convertToDTO(Price price)
+		{
+			var dtoPrice = new DTO.PriceDTO();
+
+			dtoPrice.SmallSizePrice = price.SmallSizePrice;
+			dtoPrice.MediumSizePrice = price.MediumSizePrice;
+			dtoPrice.LargeSizePrice = price.LargeSizePrice;
+			dtoPrice.RegularCrustPrice = price.RegularCrustPrice;
+			dtoPrice.ThinCrustPrice = price.ThinCrustPrice;
+			dtoPrice.ThickCrustPrice = price.ThickCrustPrice;
+			dtoPrice.SausagePrice = price.SausagePrice;
+			dtoPrice.PepperoniPrice = price.PepperoniPrice;
+			dtoPrice.OnionsPrice = price.OnionsPrice;
+			dtoPrice.GreenPeppersPrice = price.GreenPeppersPrice;
+
+			return dtoPrice;
+		}
 	}
 }
